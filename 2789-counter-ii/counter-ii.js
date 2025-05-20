68712c9fbefa199efc09097f28d01f42f17eb63e
Jonatan Badillo <120596087+JonatanBadillo@users.noreply.github.com>
2025-05-20 11:24:42 -0600

    Time: 53 ms (28.51%) | Memory: 57.5 MB (32.63%) - LeetSync

---
+++ 2789-counter-ii/counter-ii.js
@@ -0,0 +1,33 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let value = init
+    const increment = ()=>{
+        value++;
+        return value;
+    }
+
+    const reset = ()=>{
+        value = init;
+        return value;
+    }
+
+    const decrement = ()=>{
+        value--;
+        return value;
+    }
+
+    return{
+        increment, reset, decrement
+    }
+    
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
