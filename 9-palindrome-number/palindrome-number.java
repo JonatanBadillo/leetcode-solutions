a6003ebde64adaef22b41b8c8a62d461acc19bf1
Jonatan Badillo <120596087+JonatanBadillo@users.noreply.github.com>
2025-06-17 14:19:53 -0600

    Time: 6 ms (32.45%) | Memory: 44.2 MB (49.17%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,21 +1,22 @@
 class Solution {
 
-    private static boolean checkPalindrome(String num){
-        int left = 0;
-        int right = num.length() - 1;
-        while(left < right){
-            if(num.charAt(left) != num.charAt(right)){
+    private static boolean checkPalindrome(String num) {
+        int pointerLeft = 0;
+        int pointerRight = num.length() - 1;
+
+        while (pointerLeft < pointerRight) {
+            if (num.charAt(pointerLeft) != num.charAt(pointerRight)) {
                 return false;
             }
-            left++;
-            right--;
+            pointerLeft++;
+            pointerRight--;
         }
         return true;
+
     }
 
-
     public boolean isPalindrome(int x) {
-        if(x<0){
+        if (x < 0) {
             return false;
         }
         String num = x + "";
